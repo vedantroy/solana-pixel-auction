7b00a5dbba0ab1623db892fb3fbab6596073378b
Vedant Roy <vroy101@gmail.com>
2021-11-28 18:09:37 -0500

    Working createGameState

--- programs/auction/src/lib.rs
+++ programs/auction/src/lib.rs
@@ -4,25 +4,35 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
 pub mod auction {
+
     use super::*;
     pub fn create_game_state(ctx: Context<CreateGameState>, bump: u8) -> ProgramResult {
+        // I'm assuming these are set by default
         ctx.accounts.game_account.bump = bump;
-        ctx.accounts.game_account.state = 0;
+        ctx.accounts.game_account.bid_lamports = 0;
+        ctx.accounts.game_account.owner = None;
+
         Ok(())
     }
 
+    pub fn bid(ctx: Context<DoGameBid>, bid: u64) -> ProgramResult {
+        let ix = anchor_lang::solana_program::system_instruction::transfer(
+            &ctx.accounts.from.key(),
+            &ctx.accounts.game_account.key(),
+            bid,
+        );
 
-    pub fn increment_counter(ctx: Context<DoGameVote>) -> ProgramResult {
-        ctx.accounts.game_account.state += 1;
-        Ok(())
-    }
+        anchor_lang::solana_program::program::invoke(
+            &ix,
+            &[
+                ctx.accounts.from.to_account_info(),
+                ctx.accounts.game_account.to_account_info(),
+            ],
+        )?;
 
-    pub fn decrement_counter(ctx: Context<DoGameVote>) -> ProgramResult {
-        ctx.accounts.game_account.state -= 1;
         Ok(())
     }
 }
-
 
 #[derive(Accounts)]
 #[instruction(bump: u8)]
@@ -32,25 +42,39 @@ pub struct CreateGameState<'info> {
       seeds=[],
       bump=bump)]
     pub game_account: Account<'info, GameState>,
+    #[account(mut)]
     pub user: Signer<'info>,
     pub system_program: Program<'info, System>,
 }
 
 #[derive(Accounts)]
-pub struct DoGameVote<'info> {
+#[instruction(bid: u64)]
+pub struct DoGameBid<'info> {
     #[account(mut, seeds = [], bump = game_account.bump)]
     game_account: Account<'info, GameState>,
+
+    #[account(mut)]
+    from: Signer<'info>,
+
+    system_program: Program<'info, System>,
 }
 
 #[account]
 pub struct GameState {
     pub bump: u8,
-    state: usize,
+    owner: Option<Pubkey>,
+    bid_lamports: u64,
 }
 
 impl GameState {
     fn space() -> usize {
-        // discriminator + bump
-        8 + 1
+        // discriminator
+        8 +
+        // bump
+        1 +
+        // PubKey
+        32 +
+        // bid_lamports
+        1
     }
 }
--- tests/auction.ts
+++ tests/auction.ts
@@ -1,17 +1,47 @@
-import * as anchor from '@project-serum/anchor';
-import { Program } from '@project-serum/anchor';
-import { Auction } from '../target/types/auction';
+import * as anchor from "@project-serum/anchor";
+import { Program } from "@project-serum/anchor";
+import { Auction } from "../target/types/auction";
 
-describe('auction', () => {
+async function createUser(provider) {
+  // 2nd num = lamports per SOL (roughly)
+  const airdropBalance =  10 * 1_000_000_000;
+  let user = anchor.web3.Keypair.generate();
+  let sig = await provider.connection.requestAirdrop(user.publicKey, airdropBalance);
+  await provider.connection.confirmTransaction(sig);
 
+  let wallet = new anchor.Wallet(user);
+  let userProvider = new anchor.Provider(provider.connection, wallet, provider.opts);
+
+  return {
+    key: user,
+    wallet,
+    provider: userProvider,
+  };
+}
+
+describe("auction", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
+  const provider = anchor.Provider.env()
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.Auction as Program<Auction>;
 
-  it('Is initialized!', async () => {
-    // Add your test here.
-    const tx = await program.rpc.initialize({});
-    console.log("Your transaction signature", tx);
+  it("Can create game state", async () => {
+    const admin = await createUser(provider)
+
+    const [gameAccount, bump] = await anchor.web3.PublicKey.findProgramAddress(
+      [],
+      program.programId
+    );
+
+    const tx = await program.rpc.createGameState(bump, {
+      accounts: {
+        gameAccount: gameAccount,
+        user: admin.key.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      },
+      signers: [admin.key]
+    });
+
   });
 });
